46ad6cf08e7c138990676089fe6f0a49e7c3c642
YanikaD <yanika.dontong@gmail.com>
2023-10-08 20:49:33 +0700

    add dist

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-export default {
-  server: {
-    host: '0.0.0.0',
-  }
-}
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  optimizeDeps: {
+    include: ['three', 'tween.js'], // Ensure these libraries are included in the bundle
+  },
+  resolve: {
+    alias: {
+      // Map Three.js and Tween.js to their respective modules
+      three: './node_modules/three/build/three.module.js',
+      'tween.js': 'tween.js',
+    },
+  },
+});
